af0365c65b25926fadb9a3739b486b9dda34a13b
robertorj7 <robertorj7@yahoo.com>
2025-07-31 17:34:49 -0300

    add contact component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,28 @@ import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from "./components/container/container.component";
 import { HeaderComponent } from "./components/header/header.component";
 import { DividerComponent } from "./components/divider/divider.component";
+import { ContactComponent } from "./components/contact/contact.component";
+
+interface Contact {
+  id: number;
+  nome: string;
+  telefone: string;
+}
+
+import contactList from './agenda.json'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContainerComponent, ContainerComponent, HeaderComponent, DividerComponent],
+  imports: [RouterOutlet, ContainerComponent, ContainerComponent, HeaderComponent, DividerComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'indexa-al';
+  alphabet: string = 'abcdefghijklmnopqrstuvwxyz';
+  contacts: Contact[] = contactList;
+
+  filterContactsByLetter(letter: string): Contact[] {
+    return this.contacts.filter(contact => contact.nome.toLowerCase().startsWith(letter));
+  }
 }
