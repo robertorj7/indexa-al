7581ed27e88c0cf87ff1be6ee48d94b0d0259450
robertorj7 <robertorj7@yahoo.com>
2025-07-31 12:26:11 -0300

    add divider component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from "./components/container/container.component";
 import { HeaderComponent } from "./components/header/header.component";
+import { DividerComponent } from "./components/divider/divider.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContainerComponent, ContainerComponent, HeaderComponent],
+  imports: [RouterOutlet, ContainerComponent, ContainerComponent, HeaderComponent, DividerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/divider/divider.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ɵEmptyOutletComponent } from "@angular/router";
+
+@Component({
+  selector: 'app-divider',
+  standalone: true,
+  imports: [ɵEmptyOutletComponent],
+  templateUrl: './divider.component.html',
+  styleUrl: './divider.component.css'
+})
+export class DividerComponent {
+
+}
