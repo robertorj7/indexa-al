d47b74e70eeb17bb44c8f1cff7cdc4b933f229fc
robertorj7 <robertorj7@yahoo.com>
2025-08-01 13:29:23 -0300

    add form-group config

--- src/app/pages/contact-form/contact-form.component.ts
+++ src/app/pages/contact-form/contact-form.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from "../../components/container/container.component";
 import { DividerComponent } from "../../components/divider/divider.component";
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [ContainerComponent, DividerComponent],
+  imports: [ContainerComponent, DividerComponent, ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
 export class ContactFormComponent {
+  contactForm!: FormGroup
 
+  constructor() {
+    this.contactForm = new FormGroup({
+      name: new FormControl(''),
+      phone: new FormControl(''),
+      email: new FormControl(''),
+      birthday: new FormControl(''),
+      socialMedia: new FormControl(''),
+      observations: new FormControl(''),
+    });
+  }
 }
