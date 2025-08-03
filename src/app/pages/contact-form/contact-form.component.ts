e95a89e7560ffd6a457e73ff6606af666c223d51
robertorj7 <robertorj7@yahoo.com>
2025-08-03 20:53:11 -0300

    add validators

--- src/app/pages/contact-form/contact-form.component.ts
+++ src/app/pages/contact-form/contact-form.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { ContainerComponent } from "../../components/container/container.component";
 import { DividerComponent } from "../../components/divider/divider.component";
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-form',
@@ -15,12 +15,25 @@ export class ContactFormComponent {
 
   constructor() {
     this.contactForm = new FormGroup({
-      name: new FormControl(''),
-      phone: new FormControl(''),
-      email: new FormControl(''),
+      name: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.email, Validators.required]),
       birthday: new FormControl(''),
       socialMedia: new FormControl(''),
       observations: new FormControl(''),
     });
+  }
+
+  saveContact() {
+    if (this.contactForm.valid) {
+      const contactData = this.contactForm.value;
+      console.log('Contato salvo:', contactData);      
+    } else {
+      console.log('Formulário inválido');
+    }
+  }
+
+  cancel() {
+    this.contactForm.reset();
   }
 }
