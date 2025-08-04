0635d685db976edf6b85f51010a42c14ac5613e2
robertorj7 <robertorj7@yahoo.com>
2025-08-04 11:19:38 -0300

    correction button validations

--- src/app/pages/contact-form/contact-form.component.ts
+++ src/app/pages/contact-form/contact-form.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { ContainerComponent } from "../../components/container/container.component";
 import { DividerComponent } from "../../components/divider/divider.component";
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact-form',
   standalone: true,
-  imports: [ContainerComponent, DividerComponent, ReactiveFormsModule],
+  imports: [CommonModule, ContainerComponent, DividerComponent, ReactiveFormsModule],
   templateUrl: './contact-form.component.html',
   styleUrl: './contact-form.component.css'
 })
